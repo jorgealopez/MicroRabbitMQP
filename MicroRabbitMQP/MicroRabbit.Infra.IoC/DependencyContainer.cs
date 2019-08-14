1dbf0dae4d2aa25603c62b798f29623405b20ba2
jorgea.lopez <jorgea.lopez@me.com>
2019-08-14 11:25:49 -0500

    Initial Commit of Infrastructure IoC

---
+++ MicroRabbitMQP/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Infra.Bus;
+using MicroRabbitMQP.Domain.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQPBus>();
+        }
+    }
+}
